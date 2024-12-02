da8585b05a247d9771fe69affc9f64eb7c4e446b
Patrick Moreno <958patrick@gmail.com>
2024-12-02 18:54:55 +0000

    commit

--- models/staging/sql_server_dbo/stg_sql_server_dbo__addresses.sql
+++ models/staging/sql_server_dbo/stg_sql_server_dbo__addresses.sql
@@ -1,9 +1,9 @@
 {{ config(materialized='incremental') }}
 
  SELECT 
-        cast(ADDRESS_ID as varchar(36)) as ADDRESS_ID, -- todos los uuiid son de 36 caracteres.
+        CAST(ADDRESS_ID as varchar(36)) as ADDRESS_ID, -- todos los uuiid son de 36 caracteres.
         CAST( ZIPCODE AS INT) AS ZIPCODE, -- consideramos que siempre es un valor numérico
-        cast(COUNTRY as varchar(56)) as COUNTRY, -- no hay paises con más caracteres
+        CAST(COUNTRY as varchar(56)) as COUNTRY, -- no hay paises con más caracteres
         ADDRESS,
         STATE,
         CASE
